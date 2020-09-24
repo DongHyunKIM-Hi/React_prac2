f24fe9463188b34c66de5c44ddd90f9a2f76b0b0
DongHyunKIM-Hi <57423518+DongHyunKIM-Hi@users.noreply.github.com>
2020-09-24 21:36:23 +0900

    context api 만들기

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,6 +1,7 @@
-import React from "react";
-import styled from "styled-components";
-import TodoItem from "./TodoItem";
+import React from 'react';
+import styled from 'styled-components';
+import { useTodoState } from './TodoContext';
+import TodoItem from './TodoItem';
 const TodoListBlock = styled.div`
   flex: 1;
   padding: 20px 32px;
@@ -8,6 +9,8 @@ const TodoListBlock = styled.div`
   overflow-y: auto;
 `;
 function TodoList() {
+  const state = useTodoState();
+  console.log(state);
   return (
     <TodoListBlock>
       <TodoItem text="프로젝트 생성하기" done={true} />
